09395fab69eee85c0abd3ac8a7ad0caa2c62f38b
IcheVell <168340144+IcheVell@users.noreply.github.com>
2026-09-14 17:53:26 +0700

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.go
@@ -0,0 +1,42 @@
+func isPalindrome(s string) bool {
+	low := strings.ToLower(s)
+
+	runes := []rune(low)
+
+	for i, j := 0, len(runes) - 1; i < j; i, j = i + 1, j - 1 {	
+		for ; i < j && !isAlpha(runes[i]) && !isNum(runes[i]); {
+			i++
+		}
+
+		for ; j > i && !isAlpha(runes[j]) && !isNum(runes[j]) ; {
+			j--
+		}
+
+
+		if i >= j {
+			break
+		}
+
+		if runes[i] != runes[j] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func isAlpha(r rune) bool {
+	if (r >= 'A' && r <= 'Z') || (r >= 'a' && r <= 'z') {
+		return true
+	}
+
+	return false
+}
+
+func isNum(r rune) bool {
+	if r >= '0' && r <= '9' {
+		return true
+	}
+
+	return false
+}
